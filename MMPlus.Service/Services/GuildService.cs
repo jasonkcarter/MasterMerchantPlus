2dfebf312a8844d4032470c0bca9222bc03d4c66
Jason Carter <carter.jk@gmail.com>
2015-05-19 00:16:20 -0500

    Implement GuildService with repository pattern for storage.

--- MMPlus.Service/Services/GuildService.cs
+++ MMPlus.Service/Services/GuildService.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
+using MMPlus.Service.Interface;
+using MMPlus.Service.Model;
 using MMPlus.Shared.Interface;
 
 namespace MMPlus.Service.Services
@@ -9,6 +11,20 @@ namespace MMPlus.Service.Services
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class GuildService : IGuildService
     {
+        /// <summary>
+        ///     The storage data store for all Guild tables.
+        /// </summary>
+        private readonly IStorageRepository _repository;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="GuildService" /> class.
+        /// </summary>
+        /// <param name="repository">The storage data store for all Guild tables.</param>
+        public GuildService(IStorageRepository repository)
+        {
+            _repository = repository;
+        }
+
         /// <summary>
         ///     Gets a list of guild meta data records for a given list of guild names.
         /// </summary>
@@ -19,7 +35,16 @@ namespace MMPlus.Service.Services
         [OperationContract(AsyncPattern = true)]
         public IEnumerable<IEsoGuild> Get(IEnumerable<string> guildNames)
         {
-            throw new NotImplementedException();
+            var results = new List<IEsoGuild>();
+            foreach (string guildName in guildNames)
+            {
+                EsoGuild guild = _repository.Find<EsoGuild>(string.Empty, guildName).FirstOrDefault();
+                if (guild != null)
+                {
+                    results.Add(guild);
+                }
+            }
+            return results;
         }
     }
 }
---
+++ MMPlus.Test/Service/GuildServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MMPlus.Service.Model;
+using MMPlus.Service.Services;
+
+namespace MMPlus.Test.Service
+{
+    [TestClass]
+    public class GuildServiceTests
+    {
+        [TestMethod]
+        public void GuildService_Get()
+        {
+            // Arrange
+            var repository = new MemoryStorageRepository();
+            MemoryStorageTable<EsoGuild> table = repository.GetTable<EsoGuild>();
+            var guildNames = new[]
+            {
+                "Iron Bank of Braavos",
+                "Ethereal Traders Union"
+            };
+            var expectedGuildNames = new[] {guildNames[0]};
+            foreach (string guildName in guildNames)
+            {
+                table.InsertOrReplace(new EsoGuild {Name = guildName, AllConfirmedThresholdTimestampId = 20000});
+            }
+
+            // Act
+            var service = new GuildService(repository);
+            string[] results = service.Get(expectedGuildNames).Select(x => x.Name).ToArray();
+
+            // Assert
+            string[] unmatchedGuildNames = expectedGuildNames.Except(results).ToArray();
+            if (unmatchedGuildNames.Any())
+            {
+                Assert.Fail("Missing the following expected guild names: {0}", string.Join(", ", unmatchedGuildNames));
+            }
+            string[] unexpectedGuildNames = results.Except(expectedGuildNames).ToArray();
+            if (unexpectedGuildNames.Any())
+            {
+                Assert.Fail("Found the following unexpected guild names: {0}", string.Join(", ", unexpectedGuildNames));
+            }
+        }
+    }
+}
